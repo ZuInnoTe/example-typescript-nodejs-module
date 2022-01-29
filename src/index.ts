be23586f0644eaa417609fe6d8722cda45f11f88
Jörn Franke <jornfranke@gmail.com>
2022-01-29 22:16:22 +0100

    added some exmaple typedoc documentation

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,32 @@
-/** Example service */
+/** Example service
+ *
+ * ```typescript
+ * const exampleService = new ExampleService();
+ * ```
+ *
+ */
 
 export class ExampleService {
+  /**
+   * called when a new instance is created
+   */
   constructor() {
     // nothing to do
   }
 
+  /**
+   * Returns "Hello World!"
+   *
+   * Example:
+   *  ```typescript
+   * const exampleService = new ExampleService();
+   * helloWorldString = exampleService.helloWorld();
+   * ```
+   *
+   * @returns a string with "Hello World!"
+   *
+   *
+   */
   public helloWorld(): string {
     return "Hello World!";
   }
